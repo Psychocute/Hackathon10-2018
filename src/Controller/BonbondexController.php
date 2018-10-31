92fd62d18850d842fa080ef47a8fbe38b8278e5e
Candice Bourguignon <candice.bourguignon@gmail.com>
2018-10-31 04:23:33 +0100

    affichage du bonbondex

--- src/Controller/BonbondexController.php
+++ src/Controller/BonbondexController.php
@@ -7,12 +7,35 @@
  */
 
 namespace Controller;
-
+use Model\Bonbondex;
+use Model\BonbondexManager;
+use GuzzleHttp\Client;
 
 class BonbondexController extends AbstractController
-{
+{   private $uri = 'https://fr.openfoodfacts.org/cgi/search.pl';
+    private $search = 'bonbons';
+
     public function index()
     {
-          return $this->twig->render('Bonbondex/index.html.twig');
+
+
+        $client = new Client();
+        $res = $client->request('GET', 'https://fr-en.openfoodfacts.org/category/sweet/1.json');
+        //echo json_decode($res->getBody()); die();
+
+        $reponse = json_decode ($res->getBody(), true);
+
+        echo $reponse['count']."<br/><br/>";
+        $liste = $reponse['products'];
+        //echo json_decode($liste[0]);
+
+
+        //foreach($liste as $product){
+           // echo $product['product_name']."<br/>";
+           // echo $product['image_url']."<br/><br/>";
+        //}
+
+        return $this->twig->render('Bonbondex/index.html.twig',['liste' => $liste]);
     }
 }
+
---
+++ src/Model/BonbondexManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder5
+ * Date: 30/10/18
+ * Time: 21:12
+ */
+
+namespace Model;
+
+
+class BonbondexManager
+{
+
+}
